c917af7fe65e0f931c3f335e0486dbf3447c0bff
Ayoub <youbima17@gmail.com>
2023-09-14 12:54:23 +0200

    poll service initialization

---
+++ poll-service/src/main/java/fr/teama/pollservice/PollServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.teama.pollservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PollServiceApplication.class, args);
+    }
+
+}
---
+++ poll-service/src/test/java/fr/teama/pollservice/PollServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.teama.pollservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
