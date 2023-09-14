f05846da8889e214c9d58c5d92913e734c57c472
Antoine Buquet <antoine.buquet49@gmail.com>
2023-09-14 13:32:08 +0200

    docker mission service

--- start-all.sh
+++ start-all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "starting all"
-docker-compose --file weather-service/docker-compose-weather.yml up -d
+docker-compose --file weather-service/docker-compose-weather.yml \
+               --file mission-service/docker-compose-mission.yml up -d
+
+# read -p "Press any key to continue... "
 
 echo "all services started"
