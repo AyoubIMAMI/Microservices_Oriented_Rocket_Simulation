f1334bdf195097cc1fbf89655da402366768213b
Antoine Buquet <antoine.buquet49@gmail.com>
2023-09-14 15:18:23 +0200

    prepare.sh

--- start-all.sh
+++ start-all.sh
@@ -5,6 +5,7 @@ docker-compose --file weather-service/docker-compose-weather.yml \
                --file mission-service/docker-compose-mission.yml \
                --file rocket-service/docker-compose-rocket.yml up -d
 
+# remove # to add pause in script execution
 read -p "Press any key to continue... "
 
 echo "** All services started"
