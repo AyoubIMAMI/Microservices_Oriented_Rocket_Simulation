539db1d02540f67def07c625ad587ef5a518ac29
Antoine Buquet <antoine.buquet49@gmail.com>
2023-09-14 16:36:35 +0200

    better script

--- build-all.sh
+++ build-all.sh
@@ -10,15 +10,16 @@ function compile_dir()  # $1 is the dir to get it
 echo "** Building all"
 
 docker image rm marsy/weather-service
+docker image rm marsy/mission-service
+docker image rm marsy/rocket-service
+
 compile_dir "weather-service"
 
-docker image rm marsy/mission-service
 compile_dir "mission-service"
 
-docker image rm marsy/rocket-service
 compile_dir "rocket-service"
 
 # remove # to add pause in script execution
-# read -p "Press any key to continue... "
+read -p "Press any key to continue... "
 
 echo "** Done all"
