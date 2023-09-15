564111ebc3749161d937612336f81ded80ae7a75
Ayoub <youbima17@gmail.com>
2023-09-15 10:01:26 +0200

    @RequestMapping added

--- mission-service/src/main/java/fr/teama/missionservice/controllers/MissionController.java
+++ mission-service/src/main/java/fr/teama/missionservice/controllers/MissionController.java
@@ -6,16 +6,20 @@ import fr.teama.missionservice.interfaces.IPollMaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RestController
+@RequestMapping(path = MissionController.BASE_URI, produces = APPLICATION_JSON_VALUE)
 public class MissionController {
     public static final String BASE_URI = "/api/mission";
 
     @Autowired
     private IPollMaker pollMaker;
 
-    @PostMapping(path = BASE_URI + "/poll")
+    @PostMapping("/poll")
     public ResponseEntity<String> startMission() throws RocketServiceUnavailableException, WeatherServiceUnavailableException {
         return pollMaker.startMission();
     }
