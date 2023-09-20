cb890409bf687529ad76872ff6e2283fd2b9606b
Benoit Gaudet <bgaudet38@gmail.com>
2023-09-20 16:15:29 +0200

    Add service name in logs #3

--- rocket-service/src/main/java/fr/teama/rocketservice/components/LoggerComponent.java
+++ rocket-service/src/main/java/fr/teama/rocketservice/components/LoggerComponent.java
@@ -2,21 +2,25 @@ package fr.teama.rocketservice.components;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LoggerComponent {
-    public static final Logger LOG = LoggerFactory.getLogger(LoggerComponent.class);
+    private static final Logger LOG = LoggerFactory.getLogger(LoggerComponent.class);
+
+    @Value("${service.name}")
+    private String serviceName;
 
     public void logInfo(String logging) {
-        LOG.info("\u001B[32m" + logging + "\u001B[0m");
+        LOG.info("\u001B[34m" + serviceName + ": \u001B[32m" + logging + "\u001B[0m");
     }
 
     public void logWarn(String logging) {
-        LOG.warn("\u001B[33m" + logging + "\u001B[0m");
+        LOG.warn("\u001B[34m" + serviceName + ": \u001B[33m" + logging + "\u001B[0m");
     }
 
     public void logError(String logging) {
-        LOG.error("\u001B[31m" + logging + "\u001B[0m");
+        LOG.error("\u001B[34m" + serviceName + ": \u001B[31m" + logging + "\u001B[0m");
     }
 }
