069875809b7f6db8971f80993471e16fcd1ec9dd
Ayoub <youbima17@gmail.com>
2023-09-20 10:01:15 +0200

    payload-service initialization

---
+++ payload-service/src/test/java/fr/teama/payloadservice/PayloadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.teama.payloadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayloadServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
