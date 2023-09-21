0f98559a1bd8748b927f8cc6cd4a4af1e5cfe091
Ayoub <youbima17@gmail.com>
2023-09-21 12:17:50 +0200

    Ask orbit to telemetry #9

--- payload-service/src/main/java/fr/teama/payloadservice/interfaces/IDataAsker.java
+++ payload-service/src/main/java/fr/teama/payloadservice/interfaces/IDataAsker.java
@@ -1,8 +1,9 @@
 package fr.teama.payloadservice.interfaces;
 
+import fr.teama.payloadservice.exceptions.TelemetryServiceUnavailableException;
 import org.springframework.http.ResponseEntity;
 
 public interface IDataAsker {
 
-    ResponseEntity<String> askOrbitToTelemetry();
+    ResponseEntity<String> askOrbitToTelemetry() throws TelemetryServiceUnavailableException;
 }
