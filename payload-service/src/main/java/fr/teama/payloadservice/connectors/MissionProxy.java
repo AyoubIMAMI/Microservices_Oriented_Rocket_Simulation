b2315af9f368cfe68975a3cae23af4fc0e2f4f9d
Benoit Gaudet <bgaudet38@gmail.com>
2023-09-23 10:15:15 +0200

    Change payload logger component to static logger #3

--- payload-service/src/main/java/fr/teama/payloadservice/connectors/MissionProxy.java
+++ payload-service/src/main/java/fr/teama/payloadservice/connectors/MissionProxy.java
@@ -1,9 +1,8 @@
 package fr.teama.payloadservice.connectors;
 
 import fr.teama.payloadservice.exceptions.TelemetryServiceUnavailableException;
-import fr.teama.payloadservice.interfaces.ILoggerComponent;
+import fr.teama.payloadservice.helpers.LoggerHelper;
 import fr.teama.payloadservice.interfaces.proxy.IMissionProxy;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -13,17 +12,14 @@ import org.springframework.web.client.RestTemplate;
 public class MissionProxy implements IMissionProxy {
     @Value("${mission.host.baseurl}")
     private String apiBaseUrlHostAndPort;
-
-    @Autowired
-    ILoggerComponent logger;
     private final RestTemplate restTemplate = new RestTemplate();
     @Override
     public ResponseEntity<String> missionSuccessNotify() throws TelemetryServiceUnavailableException {
         try {
-            logger.logInfo("Notify mission-service that the mission that the Payload has been dropped.");
+            LoggerHelper.logInfo("Notify mission-service that the mission that the Payload has been dropped.");
             return restTemplate.postForEntity(apiBaseUrlHostAndPort + "/mission/success", null, String.class);
         } catch (Exception e) {
-            System.out.println(e);
+            LoggerHelper.logError(e.toString());
             throw new TelemetryServiceUnavailableException();
         }
     }
--- payload-service/src/main/java/fr/teama/payloadservice/helpers/LoggerHelper.java
+++ payload-service/src/main/java/fr/teama/payloadservice/helpers/LoggerHelper.java
@@ -1,11 +1,11 @@
-package fr.teama.rockethardwareservice.helpers;
+package fr.teama.payloadservice.helpers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LoggerHelper {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggerHelper.class);
-    private static final String SERVICE_NAME = "rocket-hardware-mock-service";
+    private static final String SERVICE_NAME = "payload-service";
 
     public static void logInfo(String logging) {
         LOGGER.info("\u001B[34m" + SERVICE_NAME + ": \u001B[32m" + logging + "\u001B[0m");
