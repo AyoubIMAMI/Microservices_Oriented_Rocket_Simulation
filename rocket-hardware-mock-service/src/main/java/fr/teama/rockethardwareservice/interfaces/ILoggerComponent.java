588143769bc6938184cf0ef82bed35ba0f370da9
Benoit Gaudet <bgaudet38@gmail.com>
2023-09-23 10:09:32 +0200

    Change rocket hardware logger component to static logger #3

--- rocket-hardware-mock-service/src/main/java/fr/teama/rockethardwareservice/interfaces/ILoggerComponent.java
+++
@@ -1,7 +0,0 @@
-package fr.teama.rockethardwareservice.interfaces;
-
-public interface ILoggerComponent {
-    void logInfo(String logging);
-    void logWarn(String logging);
-    void logError(String logging);
-}
