4de2104f235e33577ab50ae53a0fb28e545a81ed
Benoit Gaudet <bgaudet38@gmail.com>
2023-09-27 11:23:56 +0200

    Refractor docker compose and follow log script #24

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function run_test() {
+
+  printf "## Asking Mission Commander to start the Rocket \n"
+
+  curl --write-out '%{http_code}' --silent --show-error --output /dev/null --location --request POST http://localhost:3001/api/mission/start | ./follow-logs.sh
+  ret_code=$?
+   # Check the return code
+  if [ $ret_code -eq 0 ]; then
+    # Print the response body
+    echo "$response"
+  else
+    # Handle the error here
+    echo "Error: The request did not succeed (HTTP status code: $ret_code)"
+  fi
+}
+
+
+# echo "Running concurrency test WITH NO PRIOR BUILD"
+# echo "through the gateway"
+run_test
+
+
+read -p "** Press any key to continue... "
