5dbe4b63c6d780fedf6cf0728d55129cf5e87138
Ayoub <youbima17@gmail.com>
2023-09-27 15:58:48 +0200

    rocker hardware proxy #23

--- rocket-department-service/src/main/java/fr/teama/rocketdepartmentservice/connectors/RocketHardwareProxy.java
+++ rocket-department-service/src/main/java/fr/teama/rocketdepartmentservice/connectors/RocketHardwareProxy.java
@@ -1,11 +1,12 @@
 package fr.teama.rocketdepartmentservice.connectors;
 
+import fr.teama.rocketdepartmentservice.interfaces.proxy.IRocketHardwareProxy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
-public class RocketHardwareProxy {
+public class RocketHardwareProxy implements IRocketHardwareProxy {
     @Value("${rocket-stage.host.baseurl}")
     private String apiBaseUrlHostAndPort;
 
