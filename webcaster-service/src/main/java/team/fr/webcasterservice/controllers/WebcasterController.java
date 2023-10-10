c8b8ad6f8faa4f2f53dcb4f749029e1b4a41b021
Ayoub <youbima17@gmail.com>
2023-10-10 23:39:58 +0200

    webcaster route #34

--- webcaster-service/src/main/java/team/fr/webcasterservice/controllers/WebcasterController.java
+++ webcaster-service/src/main/java/team/fr/webcasterservice/controllers/WebcasterController.java
@@ -1,8 +1,7 @@
 package team.fr.webcasterservice.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import team.fr.webcasterservice.helpers.LoggerHelper;
 
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
@@ -12,5 +11,8 @@ import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 public class WebcasterController {
     public static final String BASE_URI = "/api/webcaster";
 
-
+    @PostMapping
+    void receiveLogs(@RequestBody String logs) {
+        LoggerHelper.logInfo("Marie: 'The following news is whispered to me: " + logs + "'");
+    }
 }
