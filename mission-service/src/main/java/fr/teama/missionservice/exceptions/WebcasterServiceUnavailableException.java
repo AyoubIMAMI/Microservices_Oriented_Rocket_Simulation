41b38f78514ecbf84f9810d88ea5d181ea8c3033
Ayoub <youbima17@gmail.com>
2023-10-10 23:54:47 +0200

    webcaster exception added #34

---
+++ mission-service/src/main/java/fr/teama/missionservice/exceptions/WebcasterServiceUnavailableException.java
@@ -0,0 +1,8 @@
+package fr.teama.missionservice.exceptions;
+
+public class WebcasterServiceUnavailableException extends Exception {
+
+    public WebcasterServiceUnavailableException() {
+
+    }
+}
