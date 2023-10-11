005711fd3104ff3f582ede34d62a10c2023cf7d9
Ayoub <youbima17@gmail.com>
2023-10-11 13:37:51 +0200

    retrieve payload data by rocket name #38

--- payload-service/src/main/java/fr/teama/payloadservice/repository/PayloadDataRepository.java
+++ payload-service/src/main/java/fr/teama/payloadservice/repository/PayloadDataRepository.java
@@ -5,6 +5,9 @@ import fr.teama.payloadservice.models.PayloadData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PayloadDataRepository extends JpaRepository<PayloadData, Long> {
+    List<PayloadData> findAllByRocketName(String rocketName);
 }
