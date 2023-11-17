ae89ac9a9e79878d91b0f17c0175ecd9e9f9b7be
MouradKarrakchou <91179433+MouradKarrakchou@users.noreply.github.com>
2023-11-17 17:58:37 +0100

    Adding a slave database to telemetry and a new Service #47

---
+++ telemetry-reader-service/src/main/java/fr/teama/telemetryreaderservice/TelemetryReaderServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.teama.telemetryreaderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemetryReaderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemetryReaderServiceApplication.class, args);
+	}
+
+}
---
+++ telemetry-reader-service/src/test/java/fr/teama/telemetryreaderservice/TelemetryReaderServiceApplicationTests.java
@@ -0,0 +1,10 @@
+package fr.teama.telemetryreaderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemetryReaderServiceApplicationTests {
+
+
+}
